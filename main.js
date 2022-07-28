5ff3c01097fdc9b8538fc3cc8d7583be767294a5
sagor ahamed <sagorahamed017997@gmail.com>
2022-07-28 18:40:55 +0600

    calculator

---
+++ main.js
@@ -0,0 +1,20 @@
+const button = document.querySelectorAll(".number");
+const display = document.querySelector(".showInputNumber");
+const displayclear = document.querySelector(".clear");
+const calculateItems = document.querySelector(".calculate")
+
+button.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        display.value += item.value;
+    })
+})
+
+displayclear.addEventListener("click", ()=>{
+    display.value = "";
+})
+
+calculateItems.addEventListener("click", ()=>{
+    const result = eval(display.value)
+    display.value = "";
+    display.value = result;
+})
